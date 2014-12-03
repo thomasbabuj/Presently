67c483863a4501cb8bf8fd5c4a774fddc073cc2e
Thomas Babu <tommy.developer@gmail.com>
2014-12-04 01:19:25 +0800

    added the service js

--- js/services.js
+++ js/services.js
@@ -1,4 +1,23 @@
+/*
+* Services persists across controllers, for the
+* Duration of the application's lifetime and its the 
+* right place to hide the business logic from the controller.  
+*/
+
 angular.module('myApp', [])
 	.provider('Weather', function() {
+		// Wunderground API key
+		var apiKey = '';
+
+		this.setApiKey = function(key) {
+			if( key ) 
+				this.apiKey = key;
+		};
+
+		this.$get = function($http) {
+			return {
+				// Service object
+			}
+		}
 
 	});
