e2d94f6ae7e21c2202d2c42598507d45b3ce841e
Thomas Babu <tommy.developer@gmail.com>
2014-12-05 23:38:14 +0800

    created a function that generate a api call url

--- js/app.js
+++ js/app.js
@@ -1,4 +1,26 @@
 angular.module('myApp', [])
+
+	.provider('Weather', function() {
+		var apiKey = '';
+
+		this.getUrl = function(type,ext) {
+			return "http://api.wunderground.com/api/"+
+				this.apiKey+ "/"+ type + "/q/" + ext + '.json';
+		};
+
+		this.setApiKey = function(key) {
+			if (key) this.apiKey = key;
+		};
+
+		this.$get =function($http) {
+			return {
+				// Service object
+			}
+		}
+	})
+	.config(function(WeatherProvider){
+		WeatherProvider.setApiKey('80c081fda40c2009');
+	})
 	.controller('MainCtrl', function($scope, $timeout) {
 		// Build date object
 		$scope.date = {};
