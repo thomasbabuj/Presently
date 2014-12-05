6308dcb6895efa49f9720b37a32db212c904105e
Thomas Babu <tommy.developer@gmail.com>
2014-12-05 23:45:51 +0800

    created our api call

--- js/app.js
+++ js/app.js
@@ -12,9 +12,26 @@ angular.module('myApp', [])
 			if (key) this.apiKey = key;
 		};
 
-		this.$get =function($http) {
+		this.$get =function($q,$http) {
+			var self = this;
 			return {
 				// Service object
+				getWeatherForecast : function(city) {
+					var d = $q.defer();
+					$http({
+						method: 'GET',
+						url : self.getUrl("forecast", city),
+						cache : true
+					}).success(function(data) {
+						// The wunderground API returns the 
+						// object that nests the forecasts inside 
+						// the forecast.simpleforecast key
+						d.resolve(data.forecast.simpleforecast);
+					}).error(function(err){
+						d.reject(err);
+					});
+					return d.promise;
+				}
 			}
 		}
 	})
